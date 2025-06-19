5a146383f65357612219d652fd4cea58ad8a2b5b
Sebxs940 <sprogramacion940@gmail.com>
2025-06-19 15:25:18 -0500

    Add the all pages, except dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import express from '@astrojs/express';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: express(),
   vite: {
     plugins: [tailwindcss()]
   }
